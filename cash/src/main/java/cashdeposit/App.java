70896b3665310c1f5588ba430cf72c8bf97e7c82
pawan <pawan.11@hydcom>
2020-12-02 16:59:18 +0530

    hdfcfile

---
+++ cash/src/main/java/cashdeposit/App.java
@@ -0,0 +1,13 @@
+package cashdeposit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
